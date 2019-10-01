5c7383a929aa34fd7a26fefc60f3b776fb562e32
a-nano <an74abc@gmail.com>
2019-10-01 17:08:43 +0900

    fixed

--- strategy/package.lisp
+++ strategy/package.lisp
@@ -1,4 +1,12 @@
 (in-package :common-lisp)
 
 (defpackage strategy
-  (:use :cl))
+  (:use :cl)
+  (:export :make-player
+	   :win
+	   :lose
+	   :even
+	   :next-hand
+	   :to-string
+	   :make-won-strategy
+	   :make-prob-strategy))
